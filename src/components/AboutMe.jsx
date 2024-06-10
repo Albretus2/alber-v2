a18a3c81e9a5af4dcc14ee9b35d1720f9f3614db
Alberr <dedesur@Albert.local>
2024-06-10 19:39:40 +0700

    📟 fixed spasi in about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,8 +3,7 @@ export default function About() {
     <div className="aboutMe mb-12">
       <p className="-mt-8 mb-4 opacity-50 text-sm">A little bit about me -</p>
       <p className="mb-4">
-        Introducing my name
-        <span className="italic">A. Ichvanius Giharta</span>, usually called
+        Introducing my name <span className="italic">A. Ichvanius Giharta</span>, usually called
         alber. I am a fresh graduate with almost 3 years of experience as a web
         developer.
       </p>
